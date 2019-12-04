07bd47e36842041029afec997e4cca540dd8b101
John Theo <john@Johns-MacBook-Pro.local>
2019-12-04 21:57:10 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import './css/index.scss';
+
+const App = () => <div className="xd">Hello world</div>;
+
+render(<App />, document.getElementById('root'));
