3b4f0b808eb6b30a71aaeedd44aa5461b55683c3
John Theo <john@Johns-MacBook-Pro.local>
2019-12-05 22:57:01 +0200

    feat: extra rules

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import * as propTypes from 'prop-types';
+
 import { render } from 'react-dom';
 
 import './css/index.scss';
@@ -7,6 +9,12 @@ interface IProps {
   title: string;
 }
 
-const App: React.FC<IProps> = props => <div className="xd">{props.title}</div>;
+const {a, b} = 5;
+
+const App: React.FC<IProps> = ({ title }) => <div className="xd">{title}</div>;
 
 render(<App title="Typescript success" />, document.getElementById('root'));
+
+App.propTypes = {
+  title: propTypes.string.isRequired,
+};
